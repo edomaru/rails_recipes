1af7e3d24937103bae5aceb198fc91c1b058cdbe
edo masaru <masaruedo@yahoo.com>
2015-06-20 17:26:38 +0700

    Run some test for recipe model and add validations on it

---
+++ test/models/recipe_test.rb
@@ -0,0 +1,57 @@
+require "test_helper"
+
+class RecipeTest < ActiveSupport::TestCase
+	def setup
+		@recipe = Recipe.new(name: "chicken parms", summary: "this is the best chicken parm recipe ever",
+					description: "heat oil, add onions, etc.")
+	end
+
+	test "reipe should be valid" do
+		assert @recipe.valid?
+	end
+
+	test "name should be present" do
+		@recipe.name = " "
+		assert_not @recipe.valid?
+	end
+
+	test "name length should not be too long" do
+		@recipe.name = "a" * 101
+		assert_not @recipe.valid?
+	end
+
+	test "name length should not be too short" do
+		@recipe.name = "aaaa"
+		assert_not @recipe.valid?
+	end
+
+	test "summary should be present" do
+		@recipe.summary = " "
+		assert_not @recipe.valid?
+	end
+
+	test "summary length should not be too long" do
+		@recipe.summary = "e" * 151
+		assert_not @recipe.valid?
+	end
+
+	test "summary length should not be too short" do
+		@recipe.summary = "eeeee"
+		assert_not @recipe.valid?
+	end
+
+	test "description must be present" do
+		@recipe.description = " "
+		assert_not @recipe.valid?
+	end
+
+	test "description should not be too long" do
+		@recipe.description = "e" * 501
+		assert_not @recipe.valid?
+	end
+
+	test "description should not be too short" do
+		@recipe.description = "e333df"
+		assert_not @recipe.valid?
+	end
+end
