56cf609b51727cf6354224d738e7210f47f31c4c
edo masaru <masaruedo@yahoo.com>
2015-06-21 02:25:59 +0700

    Run more test for email in chef model and add validation to it

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,4 +1,10 @@
 class Chef < ActiveRecord::Base
+	before_save { self.email = email.downcase }
+
 	validates :chefname, presence: true, length: { minimum: 3, maximum: 40 }
-	validates :email, presence: true
+
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 105 }, 
+					  uniqueness: { case_sensitive: false }, 
+					  format: { with: VALID_EMAIL_REGEX }
 end
--- test/models/chef_test.rb
+++ test/models/chef_test.rb
@@ -29,5 +29,33 @@ class ChefTest < ActiveSupport::TestCase
   	assert_not @chef.valid?
   end
 
+  test "email length should be within bound" do
+  	@chef.email = "a" * 101 + "@example.com"
+  	assert_not @chef.valid?
+  end
+
+  test "email address should be unique" do
+  	dup_chef = @chef.dup
+  	dup_chef.email = @chef.email.upcase
+  	@chef.save
+
+  	assert_not dup_chef.valid?
+  end
+
+  test "email validation should accept valid addresses" do
+  	valid_addresses = %w(use@eee.com R_TDD-S@eee.hallo.org user@example.com first.last@eee.au laura+joe@joe.monk.cm)
+  	valid_addresses.each do |email|
+  		@chef.email = email
+  		assert @chef.valid?, "#{email.inspect} should be valid"
+  	end
+  end
+
+  test "email validation should reject invalid addresses" do
+  	invalid_addresses = %w(user@example,com user_at_eee.org user.name@example eee@i_am_.com foo@eee+aar.com)
+  	invalid_addresses.each do |email|
+  		@chef.email = email
+  		assert_not @chef.valid?, "#{email.inspect} should be invalid"
+  	end
+  end
   
 end
