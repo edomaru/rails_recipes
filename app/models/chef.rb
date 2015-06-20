353b9e41051f35d2adf37cf75387a68a0a8c5805
edo masaru <masaruedo@yahoo.com>
2015-06-20 18:44:25 +0700

    Run test for chef model and add validation to it

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,2 +1,4 @@
 class Chef < ActiveRecord::Base
+	validates :chefname, presence: true, length: { minimum: 3, maximum: 40 }
+	validates :email, presence: true
 end
--- test/models/chef_test.rb
+++ test/models/chef_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class ChefTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@chef = Chef.new(chefname: "john", email: "john@example.com")
+  end
+
+  test "chefname should be valid" do
+  	assert @chef.valid?
+  end
+
+  test "chefname should be present" do
+  	@chef.chefname = " "
+  	assert_not @chef.valid?
+  end
+
+  test "chefname should not be too short" do
+  	@chef.chefname = "a"
+  	assert_not @chef.valid?
+  end
+
+  test "chefname should not be too long" do
+  	@chef.chefname = "a" * 41
+  	assert_not @chef.valid?
+  end
+
+  test "email should be present" do
+  	@chef.email = " "
+  	assert_not @chef.valid?
+  end
+
+  
 end
